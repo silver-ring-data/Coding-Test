7f5d1f8a20e705caeaf3d3662b50453d388fd39d
Silver Ring <cindypink17@gmail.com>
2026-02-26 22:35:40 +0900

    04_third_code_SUCCESS.py

--- even_or_odd.py
+++ even_or_odd.py
@@ -14,25 +14,10 @@ def solution(num):
     """
     if num % 2 == 0 :
         answer = "Even"
-    elif num % 2 == 1 :
-        answer = "Odd"
     else :
-        return 
-    return print(answer)
+        answer = "Odd"
+    return answer
 
 """
-🛠️ 틀린 부분 찾아보기 (디버깅)
-
-1. return print(answer)의 함정
-**print()**는 모니터에 글자를 보여주는 '출력' 기능일 뿐이야.
-
-**return**은 함수가 계산한 최종 결과를 '반환'해서 컴퓨터에게 돌려주는 거야.
-
-파이썬에서 print() 함수 자체는 아무것도 돌려주지 않아서(None), return print(answer)라고 쓰면 함수는 결국 None을 반환하게 돼. 프로그래머스는 채점할 때 이 반환값을 보거든!
-
-2. else: return의 문제
-정수는 2로 나누면 나머지가 0 아니면 1밖에 없지? 그래서 사실 else까지 갈 일이 거의 없어.
-
-하지만 만약 else에 걸려서 return만 하고 아무 값도 안 써주면, 이때도 함수는 None을 돌려줘서 오답 처리가 된단다.
 """
 
