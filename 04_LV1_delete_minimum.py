d16b86e751e780eec37614da11f4724128d0b615
Silver Ring <cindypink17@gmail.com>
2026-03-01 06:34:48 +0900

    Update 04_LV1_delete_minimum.py

--- 04_LV1_delete_minimum.py
+++ 04_LV1_delete_minimum.py
@@ -23,7 +23,11 @@ def solution(arr):
                 min_num = num
         index = index + 1
     
-    answer = arr.remove(min_num)
+    arr.remove(min_num)
                 
-    return answer if answer != [] else [-1]
-    
+    return arr if arr != [] else [-1]
+"""
+파이썬 리스트의 remove()는 리스트 자체를 **'수정'**하는 일을 하지만, 수정된 리스트를 우리에게 다시 **'전달(return)'**해주지는 않아.
+    my_list.remove(2)를 실행하면: my_list라는 변수가 가리키는 실제 데이터 바구니에서 2가 사라짐. (성공!)
+    result = my_list.remove(2)라고 쓰면: remove는 아무것도 반환하지 않기 때문에 result에는 None이 들어감. (함정!)
+"""
